5f48c40b238ca8a9d46ec23879505126422687c9
cattcat <78782049+cattcat@users.noreply.github.com>
2021-02-09 13:37:44 +0800

    Add files via upload

---
+++ webtestbuy.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from time import sleep
+from selenium.webdriver.common.action_chains import ActionChains
+
+
+product="動物森友會"
+
+driver=webdriver.Chrome()
+driver.get("https://shopping.pchome.com.tw/")
+element = driver.find_element_by_id("keyword")
+element.send_keys(product)
+element=driver.find_element_by_id("doSearch")
+element.click()
+sleep(3)
+element = driver.find_element_by_xpath("//*[@id='QABG0D-A900A79QA']/dd[2]/h5/a")
+sleep(2)
+element.click()
+sleep(5)
+
